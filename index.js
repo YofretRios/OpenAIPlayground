c80ae9f1512178bd08a6feee75bc5ec38790a78d
YofretRios <riosmerca28@gmail.com>
2023-04-23 23:58:26 -0500

    Remove comments

--- index.js
+++ index.js
@@ -7,16 +7,4 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-// (async function createCompletion() {
-  
-//   const response = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: "You are now a Wine Expert, Make a List of wines brand under $20 that pairs well with Carbonara, Include the URL of the product and price, only include product from Firstleaf.com",
-//     temperature: 0,
-//     max_tokens: 1000,
-//   });
-
-//   console.log(response.data);
-// })();
-
 export { openai }; 
